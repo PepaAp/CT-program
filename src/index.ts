fcd7bf98217a9f1e6dd0bb97426fee06b5c80a9d
Cupomaz <cupomaz@gmail.com>
2025-06-12 23:05:02 +0200

    add multiple programs

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,30 @@
 import express from 'express';
-const axios = require('axios');
+import axios from 'axios';
 const app = express();
 const PORT: number = 3000;
 
 app.use(express.static('public'));
-app.use(express.static('dist'));
+app.use("/dist", express.static('dist'));
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-    
 });
 
-app.get('/frontend-script', (req, res) => {
-    res.sendFile(__dirname + '/program.js');
-})
-
-app.get("/ct", (req, res) => {
-    const d: Date = new Date();    
+app.get("/ct/:program", (req, res) => {
+    const d: Date = new Date();
 
     axios.get('https://www.ceskatelevize.cz/services-old/programme/xml/schedule.php', {
         params: {
             user: "test",
             date: `${d.getDate()}.${(d.getMonth() + 1).toString().padStart(2, "0")}.${d.getFullYear()}`,
-            channel: "ct1",
+            channel: `ct${req.params["program"]}`,
             json: 1
         }
     })
-    .then(function (response) {
-        res.send(response.data.porad);
-    })
+        .then(function (response) {
+            res.send(response.data.porad);
+        })
 });
 
 
--- src/program.ts
+++ src/program.ts
@@ -1,41 +1,41 @@
-const card = document.getElementById('card');
+const cards = document.getElementById('cards');
 let count: number = 0;
 
 function dateToMin(): number {
-    const d: Date = new Date();        
+    const d: Date = new Date();
     return d.getMinutes() + d.getHours() * 60;
 }
 
-function timeToMin(datal, i:number): number {
+function timeToMin(datal, i: number): number {
     return Number(datal[i].cas.slice(0, 2)) * 60 + Number(datal[i].cas.slice(3, 5));
 }
 
 async function findActiveProgramm(datal, count: number) {
-    for (let i = 0; i < datal.length; i++) {        
+    for (let i = 0; i < datal.length; i++) {
         if (dateToMin() <= timeToMin(datal, i)) {
             return i;
         }
     }
 }
 
-async function scrollToTime(datal, count:number):Promise<void> {
-    const i = await findActiveProgramm(datal, count);   
+async function scrollToTime(datal, count: number): Promise<void> {
+    const i = await findActiveProgramm(datal, count);
     window.location.href = `#${i}`;
 }
 
-async function loadData() {
-    const url: string = "/ct"
+async function loadData(tvProgram: number) {
+    const url: string = `/ct/${tvProgram}`;
 
-    try{
+    try {
         const data = await fetch(url);
         const datal = await data.json();
         let programs = '';
         datal.forEach((card, index: number) => {
-                if (JSON.stringify(card.noticka) === '{}') {
-                    card.noticka = ''
-                }
-                const minutes = card.stopaz.slice(0, 3).replace(/^0+/, '');
-                programs += `
+            if (JSON.stringify(card.noticka) === '{}') {
+                card.noticka = ''
+            }
+            const minutes = card.stopaz.slice(0, 3).replace(/^0+/, '');
+            programs += `
                     <h4 id="${index}">${card.cas}</h4>             
                     <div class="card">
                         <img class="miniatur" src="${card.obrazky.nahled}" onerror="this.style.display='none'">
@@ -44,18 +44,33 @@ async function loadData() {
                         <p class="description">${card.noticka}</p>
                     </div>
                 `
-                count = index;
-            });
-        if (card) {
-            card.innerHTML = programs;
-        }
-        return datal;
+            count = index;
+        });
+
+        return programs;
     } catch (error) {
         console.error(error);
     }
 }
 
+async function createMultiplePrograms() {
+    const tvPrograms: number[] = [1, 2, 24, 4, 5, 6];
+
+    for (const program of tvPrograms) {
+        const data = await loadData(program);
+        const programHTML = document.createElement("div");
+
+        programHTML.className = "col-md";
+        programHTML.id = "card";
+        programHTML.innerHTML = `<div class="col-md" id="card"></div>`;
+        programHTML.children[0].innerHTML += data;
+
+        cards?.appendChild(programHTML);
+    }
+}
+
 (async () => {
-    let datal = await loadData();
-    await scrollToTime(datal, count);
+    // let datal = await loadData();
+    // await scrollToTime(datal, count);
+    await createMultiplePrograms();
 })();
